d04b5c173ed2e7f9a08c177fcc13c0117ae6b84f
Saleck-Eljili <salek.eljili@gmail.com>
2023-02-04 23:05:55 +0100

    fix: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
             "Back-end developer",
             "Full-stack Developer",
             "NodeJS",
-            "Typscript",
+            "Typescript",
             "ReactJS",
             "Java",
             "Symfony",
