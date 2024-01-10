4cf0b37f97b4eeed05c16a0c6de08f4e3fae2441
saleck1 <saleck@qwest.tv>
2024-01-10 20:04:52 +0100

    fix: node version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./App.css"
-import About from './components/container/About/About'
-import Contact from './components/container/Contact/Contact'
-import Footer from './components/container/Footer/Footer'
-import Home from './components/container/Home/Home'
-import Portfolio from './components/container/Portfolio/Portfolio'
-import Skills from './components/container/Skills/Skills'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import "./App.css";
+import About from "./components/container/About/About";
+import Contact from "./components/container/Contact/Contact";
+import Footer from "./components/container/Footer/Footer";
+import Home from "./components/container/Home/Home";
+import Portfolio from "./components/container/Portfolio/Portfolio";
+import Skills from "./components/container/Skills/Skills";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
@@ -19,7 +19,7 @@ const App = () => {
       <Contact />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
